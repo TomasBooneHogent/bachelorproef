0ecb7442c3857a749daa24a0e304444d8bcbc277
Tomas Boone <tomas.boone@student.hogent.be>
2026-04-09 14:24:42 +0200

    draft

--- bachproef/staging2.sql
+++
@@ -1,5 +0,0 @@
-  -- RATIO CALCULATIONS
-        (f.Code_9903 + f.Cost_Depreciation + f.Code_631_4 + f.Code_635_8 + f.Cost_Services +
-         f.Code_65_66B + f.Code_66A + f.Code_66B - f.Code_76A - f.Code_76B) AS Calc_CashFlow_PreTax,
-
-        (f.Equity - f.Code_20 - f.Code_21 - f.Code_12) AS Calc_Adjusted_Equity
